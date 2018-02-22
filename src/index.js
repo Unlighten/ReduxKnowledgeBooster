416c9243952abc7f486bf53d78ead8567809ed42
Aaron Moore <x2kicker2x@gmail.com>
2018-02-22 11:48:21 -0500

    React stuff

--- src/index.js
+++ src/index.js
@@ -1,15 +1,51 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import React, { Component } from 'react'
+import ReactDOM from 'react-dom'
+import YTSearch from 'youtube-api-search'
+import SearchBar from './components/search_bar'
+import VideoList from './components/video_list'
+import VideoDetail from './components/video_detail'
+import _ from 'lodash'
+const API_KEY = 'AIzaSyCcZMc3oluWWr_zpDplDcwsv7M0AGEkMJs'
 
-import App from './components/app';
-import reducers from './reducers';
+//A quick understanding of Redux - Redux is predictable state container for JS applications
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+//we use fat arrow because it can change the instance of THIS
+class App extends Component {
+    constructor(props) {
+        super(props)
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+        this.state = { 
+            videos: [],
+            selectedVideo: null
+        }
+
+        this.videoSearch('world of warcraft')
+    }
+
+    videoSearch(term) {
+        YTSearch({key: API_KEY, term: term}, (videos) => {
+            this.setState({
+                videos: videos,
+                selectedVideo: videos[0]
+            })
+        })
+    }
+
+    render() {
+        //this is lodash throttling the function to trigger once every 3 milliseconds
+        //SearchBar can still update as often it wants, it is just this one specific function that is throttled
+        const videoSearch = _.debounce((term) => { this.videoSearch(term) } , 300)
+        
+        return (
+            <div>
+                <SearchBar onSearchTermChange={videoSearch} />
+                <VideoDetail video={this.state.selectedVideo}/>
+                <VideoList
+                 onVideoSelect={selectedVideo => this.setState({selectedVideo})}
+                 videos={this.state.videos} />
+            </div>
+        )
+    }
+}
+
+ReactDOM.render(<App />, document.querySelector('.container'))
